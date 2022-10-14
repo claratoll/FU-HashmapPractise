a1d9b50f119b6d61f887f6279e0938d7f42f6566
claratoll <clara.toll@gmail.com>
2022-10-14 14:45:01 +0200

    next lexicon

--- src/Lexicon.java
+++ src/Lexicon.java
@@ -1,2 +1,18 @@
-public class Library {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Lexicon {
+    public static void Lexicon(){
+        HashMap<String, List<String>> lex = new HashMap<>();
+
+        lex.put("hej", new ArrayList<String>(Arrays.asList("hi", "hello", "hi")));
+        lex.put("middag", new ArrayList<String>(Arrays.asList("dinner", "supper")));
+
+
+        for (String key : lex.keySet()) {
+            System.out.println(key + " " + lex.get(key));
+        }
+    }
 }
--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class PhoneNumber {
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PhoneNumber {
+    public static void phoneNumber() {
+        HashMap<String, String> phoneNumber = new HashMap<>();
+
+        phoneNumber.put("Greta", "54334311533");
+        phoneNumber.put("Karin", "63423412344");
+        phoneNumber.put("Kevin", "32454301222");
+
+
+        System.out.println("Write a name on the list");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println(phoneNumber.get(name));
+
+        System.out.println("Add a name to the list:");
+        String newName = sc.nextLine();
+        System.out.println("Write the phone number");
+        String newNumber = sc.nextLine();
+
+        phoneNumber.put(newName, newNumber);
+        for (String key : phoneNumber.keySet()) {
+            System.out.println(key + " " + phoneNumber.get(key));
+
+        }
+    }
 }
+
+
